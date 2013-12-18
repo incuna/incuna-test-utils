06f5236683543939024779f9490f3c7a476bb8a6
Ian Foote <ian@incuna.com>
2013-12-18 11:59:16 +0000

    Add version and author to setup.py.

--- setup.py
+++ setup.py
@@ -1,16 +1,14 @@
 from setuptools import setup, find_packages
 
-import incuna_test_utils
-
 
 setup(
-    name='incuna-test-utils'
+    name='incuna-test-utils',
     packages=find_packages(),
     include_package_data=True,
-    version=incuna_test_utils.__version__,
+    version='0.1',
     description='',
     long_description=open('README.rst').read(),
-    author=incuna_test_utils.__author__,
+    author='Incuna',
     author_email='admin@incuna.com',
     url='https://github.com/incuna/incuna-test-utils/',
     install_requires=[],
