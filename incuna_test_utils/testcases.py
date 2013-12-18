8af8dd5f0ece02bd0cfcdf3840d528da8f08db4d
Ian Foote <ian@incuna.com>
2013-12-18 11:23:54 +0000

    Add APIRequestTestCase and UserFactory.
    Taken from incuna-epatient-api.

--- incuna_test_utils/testcases.py
+++ incuna_test_utils/testcases.py
@@ -1,5 +1,9 @@
+from django.contrib.auth.models import AnonymousUser
 from django.db.models.base import ModelBase
 from django.test import TestCase
+from rest_framework.test import APIRequestFactory, force_authenticate
+
+from . import factories
 
 
 class AbstractModelTestCase(TestCase):
@@ -19,3 +23,22 @@ object in the database.
             (self.mixin,),
             {'__module__': self.mixin.__module__},
         )
+
+
+class APIRequestTestCase(TestCase):
+    user_factory = factories.UserFactory
+
+    def create_request(self, method='get', *, url='/', user=None, auth=True, **kwargs):
+        if not user:
+            if auth:
+                user = self.user_factory.create()
+            else:
+                user = AnonymousUser()
+        kwargs['format'] = 'json'
+        request = getattr(APIRequestFactory(), method)(url, **kwargs)
+        request.user = user
+        if auth:
+            force_authenticate(request, user)
+        if 'data' in kwargs:
+            request.DATA = kwargs['data']
+        return request
