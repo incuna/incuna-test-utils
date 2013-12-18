b5dea6ff7406e1713a4ae6c41b96c98c7df48e5a
Ian Foote <ian@incuna.com>
2013-12-18 16:23:50 +0000

    Convert AbstractModelTestCase to Mixin.
    Add tearDown method to avoid model caching issues.

--- incuna_test_utils/testcases/abstract_model.py
+++ incuna_test_utils/testcases/abstract_model.py
@@ -1,21 +1,27 @@
 from django.db.models.base import ModelBase
-from django.test import TestCase
+from django.db.models.loading import cache
 
 
-class AbstractModelTestCase(TestCase):
+class AbstractModelMixin(object):
     """
-Base class for tests of model mixins. To use, subclass and specify
-the mixin class variable. A model using the mixin will be made
-available in self.model.
+    Mixin class for tests of model mixins. To use, subclass and specify
+    the mixin class variable. A model using the mixin will be made
+    available in self.model.
 
-From http://michael.mior.ca/2012/01/14/unit-testing-django-model-mixins/
-via http://stackoverflow.com/a/9678200/400691, modified as we don't need an
-object in the database.
-"""
+    From http://michael.mior.ca/2012/01/14/unit-testing-django-model-mixins/
+    via http://stackoverflow.com/a/9678200/400691, modified as we don't need an
+    object in the database.
+    """
     def setUp(self):
         # Create a dummy model which extends the mixin
+        self._model_name = '__TestModel__' + self.mixin.__name__
         self.model = ModelBase(
-            '__TestModel__' + self.mixin.__name__,
+            self._model_name,
             (self.mixin,),
             {'__module__': self.mixin.__module__},
         )
+
+    def tearDown(self):
+        # Ensure we don't leave our test model in the model cache
+        articles = cache.app_models['articles']
+        del articles[self._model_name.lower()]
