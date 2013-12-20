819e34fb8cd60a25b7796508f72a1e9ba00b5faf
Ian Foote <ian@incuna.com>
2013-12-20 17:01:38 +0000

    Fix UserFactory for django 1.4

--- incuna_test_utils/factories/user.py
+++ incuna_test_utils/factories/user.py
@@ -1,9 +1,13 @@
 import factory
 
-from django.contrib.auth import get_user_model
+try:
+    from django.contrib.auth import get_user_model
+    User = get_user_model()
+except ImportError:  # Django 1.4
+    from django.contrib.auth.models import User
 
 
 class UserFactory(factory.DjangoModelFactory):
-    FACTORY_FOR = get_user_model()
+    FACTORY_FOR = User
     email = factory.Sequence(lambda i: 'email{}@example.com'.format(i))
     name = factory.Sequence(lambda i: 'Test User {}'.format(i))
