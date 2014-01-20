78998129cba374cb72e58aaa7e4e7a77145f3918
Max Peterson <max@incuna.com>
2014-01-20 09:59:33 +0000

    Add RequestTestCase.

--- incuna_test_utils/testcases/api_request.py
+++ incuna_test_utils/testcases/api_request.py
@@ -1,24 +1,21 @@
-from django.contrib.auth.models import AnonymousUser
-from django.test import TestCase
 from rest_framework.test import APIRequestFactory, force_authenticate
 
-from .factories.user import UserFactory
+from .request import RequestTestCase
 
 
-class APIRequestTestCase(TestCase):
-    user_factory = UserFactory
+class APIRequestTestCase(RequestTestCase):
+    request_factory = APIRequestFactory
 
     def create_request(self, method='get', url='/', user=None, auth=True, **kwargs):
-        if not user:
-            if auth:
-                user = self.user_factory.create()
-            else:
-                user = AnonymousUser()
         kwargs['format'] = 'json'
-        request = getattr(APIRequestFactory(), method)(url, **kwargs)
-        request.user = user
+        request = super(APIRequestTestCase, self).create_request(
+            method=method,
+            url=url,
+            user=user,
+            auth=auth,
+            **kwargs)
         if auth:
-            force_authenticate(request, user)
+            force_authenticate(request, request.user)
         if 'data' in kwargs:
             request.DATA = kwargs['data']
         return request
