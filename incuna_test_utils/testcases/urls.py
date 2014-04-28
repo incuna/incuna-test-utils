4c789e1bf1f71c15ba5918b4b5793af5d854551d
Adam Thomas <sortoflikechess@gmail.com>
2014-04-28 09:45:25 +0100

    Add new, slightly different URL mixin
    * Refactor the existing code to make the new functionality
      slot in nicely.
    * Add a slightly different URLsMixin to deal with method-
      based views as opposed to class-based views.  The two
      mixins derive from a shared base class and are used
      in the same way.
    * 'URLsMixin' and 'URLsTestCase' do the same thing as
      before, for the sake of compatibility.

--- incuna_test_utils/testcases/urls.py
+++ incuna_test_utils/testcases/urls.py
@@ -2,10 +2,10 @@ from django.core.urlresolvers import resolve, reverse
 from django.test import TestCase
 
 
-class URLsMixin(object):
+class URLsMixinBase(object):
     """A TestCase Mixin with a check_url helper method for testing urls"""
 
-    def check_url(self, view_class, expected_url, url_name,
+    def check_url(self, view, expected_url, url_name,
                   url_args=None, url_kwargs=None):
         """
         Assert a view's url is correctly configured
@@ -17,9 +17,36 @@ class URLsMixin(object):
         reversed_url = reverse(url_name, args=url_args, kwargs=url_kwargs)
         self.assertEqual(reversed_url, expected_url)
 
+        self.assertViewNames(view, expected_url)
+
+    def assertViewNames(self, view, expected_url):
+        """
+        Assert that the view method/class that the URL resolves to is the
+        correct one.
+        """
+        pass
+
+
+class URLsMixinForViewMethod(URLsMixinBase):
+    """For testing method-based views."""
+
+    def assertViewNames(self, view_method, expected_url):
+        resolved_view_method = resolve(expected_url).func
+        self.assertEqual(resolved_view_method.__name__, view_method.__name__)
+
+
+class URLsMixinREST(URLsMixinBase):
+    """For testing class-based views."""
+
+    def assertViewNames(self, view_class, expected_url):
         resolved_view_class = resolve(expected_url).func.cls
         self.assertEqual(resolved_view_class, view_class)
 
 
-class URLsTestCase(URLsMixin, TestCase):
+class URLsMixin(URLsMixinREST):
+    """For backwards compatibility."""
+    pass
+
+
+class URLsTestCase(URLsMixinREST, TestCase):
     pass
