650ae9cc8aef904868abcefc7c405c1749ad7836
Charlie Denton <charlie@meshy.co.uk>
2014-05-15 15:21:20 +0100

    Add django 1.7 compat goodies

---
+++ incuna_test_utils/compat.py
@@ -0,0 +1,16 @@
+import django
+
+
+DJANGO_LT_17 = django.VERSION < (1, 7)
+
+
+def wipe_id_fields_on_django_lt_17(fields):
+    """
+    Remove fields ending in '_id' on Django < 1.7
+
+    This required for tests checking the list of fields on a model because in
+    Django 1.7, all the FKs appear as `other_model` and `other_model_id`.
+    """
+    if DJANGO_LT_17:
+        return filter(lambda n: not n.endswith('_id'), fields)
+    return fields
