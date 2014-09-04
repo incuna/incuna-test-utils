43c9f489a20c605bbe1994c1a8273f81452726db
Ian Foote <ian@incuna.com>
2014-09-04 16:03:48 +0100

    Add tests for Python2CountEqualMixin

--- tests/test_compat.py
+++ tests/test_compat.py
@@ -18,3 +18,21 @@ def test_wipe_id_fields_gte_17():
     with patch.object(compat, 'DJANGO_LT_17', new=False):
         wiped_fields = compat.wipe_id_fields_on_django_lt_17(fields)
         assert wiped_fields == fields
+
+
+def test_python_2_count_equal():
+    class Python2TestCase(compat.Python2CountEqualMixin):
+        def assertItemsEqual(self, first, second):
+            pass
+
+    testcase = Python2TestCase()
+    assert hasattr(testcase, 'assertCountEqual')
+
+
+def test_python3_count_equal():
+    class Python3TestCase(compat.Python2CountEqualMixin):
+        def assertCountEqual(self, first, second):
+            pass
+
+    testcase = Python3TestCase()
+    assert hasattr(testcase, 'assertCountEqual')
