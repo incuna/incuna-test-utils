a2c818527ecf1aeae516173fba6fabbaec920c54
Ian Foote <ian@incuna.com>
2014-09-04 12:11:37 +0100

    Use pytest syntax to reduce boilerplate
    * Replace assert methods with simple assert statements

--- tests/test_request_testcase.py
+++ tests/test_request_testcase.py
@@ -2,6 +2,7 @@ from django.contrib.auth.models import AnonymousUser, User
 from django.contrib import messages
 from django.contrib.sessions.backends.base import SessionBase
 from django.core.exceptions import ImproperlyConfigured
+import pytest
 
 from incuna_test_utils.testcases.request import (
     BaseRequestTestCase,
@@ -15,49 +16,49 @@ class RequestTestCase(BaseRequestTestCase):
 
     def test_create_request(self):
         request = self.create_request()
-        self.assertEqual(request.method, 'GET')
-        self.assertEqual(request.path, '/')
-        self.assertIsInstance(request.user, User)
-        self.assertIsInstance(request._messages, DummyStorage)
+        assert request.method == 'GET'
+        assert request.path == '/'
+        assert isinstance(request.user, User)
+        assert isinstance(request._messages, DummyStorage)
 
     def test_create_request_post(self):
         request = self.create_request('post')
-        self.assertEqual(request.method, 'POST')
+        assert request.method == 'POST'
 
     def test_create_request_url(self):
         url = '/foo/'
         request = self.create_request(url=url)
-        self.assertEqual(request.path, url)
+        assert request.path == url
 
     def test_create_request_user(self):
         user = UserFactory.build()
         request = self.create_request(user=user)
-        self.assertEqual(request.user, user)
+        assert request.user == user
 
     def test_create_request_anonymous(self):
         request = self.create_request(auth=False)
-        self.assertIsInstance(request.user, AnonymousUser)
+        assert isinstance(request.user, AnonymousUser)
 
     def test_create_request_data(self):
         data = {'foo': 'bar'}
         request = self.create_request(data=data)
-        self.assertEqual(request.DATA, data)
+        assert request.DATA == data
 
     def test_create_user(self):
         user = self.create_user()
-        self.assertIsInstance(user, User)
+        assert isinstance(user, User)
 
     def test_create_user_anonymous(self):
         user = self.create_user(auth=False)
-        self.assertIsInstance(user, AnonymousUser)
+        assert isinstance(user, AnonymousUser)
 
     def test_add_session_to_request(self):
         request = self.create_request()
         self.add_session_to_request(request)
-        self.assertIsInstance(request.session, SessionBase)
+        assert isinstance(request.session, SessionBase)
 
     def test_get_view(self):
-        with self.assertRaises(ImproperlyConfigured):
+        with pytest.raises(ImproperlyConfigured):
             self.get_view()
 
 
@@ -70,7 +71,7 @@ class RequestTestCaseFunctionView(BaseRequestTestCase):
 
     def test_get_view(self):
         view = self.get_view()
-        self.assertEqual(view, function_view)
+        assert view == function_view
 
 
 class ClassView:
@@ -84,7 +85,7 @@ class RequestTestCaseClassView(BaseRequestTestCase):
 
     def test_get_view(self):
         view = self.get_view()
-        self.assertEqual(view, function_view)
+        assert view == function_view
 
 
 class TestDummyStorage:
