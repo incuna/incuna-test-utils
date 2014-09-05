7cf1ece9982a4e73e225ae61f40b7b141bb356a5
Ian Foote <ian@incuna.com>
2014-09-05 09:29:22 +0100

    Refactor test_integration to use a fixture
    * Create an instance of IntegrationTestCase and pass it into each test
    * Use my_view function from tests.views instead of view

--- tests/testcases/test_integration.py
+++ tests/testcases/test_integration.py
@@ -5,48 +5,53 @@ import pytest
 
 from incuna_test_utils.testcases.integration import BaseIntegrationTestCase
 from tests.factories import UserFactory
+from tests.views import my_view
 
 
-def view(request, *args, **kwargs):
-    return HttpResponse('')
+@pytest.fixture(scope='module')
+def simple_integration():
+    class IntegrationTestCase(BaseIntegrationTestCase):
+        user_factory = UserFactory
+        view = my_view
+
+    return IntegrationTestCase()
 
 
-class IntegrationTestCase(BaseIntegrationTestCase):
-    user_factory = UserFactory
-    view = view
-
-    def test_access_view(self):
-        response = self.access_view()
+class TestIntegration:
+    @pytest.mark.django_db
+    def test_access_view(self, simple_integration):
+        response = simple_integration.access_view()
         assert isinstance(response, HttpResponse)
 
         request = response.request
         assert isinstance(request, WSGIRequest)
         assert isinstance(request.session, SessionBase)
 
-    def test_access_view_request(self):
-        request = self.create_request()
-        response = self.access_view(request=request)
+    @pytest.mark.django_db
+    def test_access_view_request(self, simple_integration):
+        request = simple_integration.create_request()
+        response = simple_integration.access_view(request=request)
 
         assert response.request == request
 
         with pytest.raises(AttributeError):
             response.request.session
 
-    def test_assert_count(self):
+    def test_assert_count(self, simple_integration):
         haystack = [1, 1, 3, 1, 2, 4]
         needle = 1
         count = 3
-        self.assert_count(needle, haystack, count)
+        simple_integration.assert_count(needle, haystack, count)
 
-    def test_assert_count_fail(self):
+    def test_assert_count_fail(self, simple_integration):
         haystack = [1, 1, 3, 1, 2, 4]
         needle = 1
         count = 4
 
         with pytest.raises(AssertionError):
-            self.assert_count(needle, haystack, count)
+            simple_integration.assert_count(needle, haystack, count)
 
-    def test__assert_count_message(self):
+    def test__assert_count_message(self, simple_integration):
         haystack = [1, 1, 3, 1, 2, 4]
         needle = 1
         count = 1
@@ -57,10 +62,10 @@ class IntegrationTestCase(BaseIntegrationTestCase):
         expected_message = expected_message.format(*expected_message_args)
 
         message_args = (needle, haystack, count, actual_count)
-        message = self._assert_count_message(*message_args)
+        message = simple_integration._assert_count_message(*message_args)
         assert message == expected_message
 
-    def test__assert_count_message_plural(self):
+    def test__assert_count_message_plural(self, simple_integration):
         haystack = [1, 1, 3, 1, 2, 4]
         needle = 1
         count = 4
@@ -71,5 +76,5 @@ class IntegrationTestCase(BaseIntegrationTestCase):
         expected_message = expected_message.format(*expected_message_args)
 
         message_args = (needle, haystack, count, actual_count)
-        message = self._assert_count_message(*message_args)
+        message = simple_integration._assert_count_message(*message_args)
         assert message == expected_message
