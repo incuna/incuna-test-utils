30732c1a8fde6e1cb45c6670d4cb46bc598b9bbe
Kevin Etienne <kevin@incuna.com>
2014-11-19 11:38:43 +0000

    Update factory raw_password

--- tests/factories.py
+++ tests/factories.py
@@ -3,14 +3,14 @@ import factory
 
 
 class UserFactory(factory.DjangoModelFactory):
+    username = factory.Sequence('User {}'.format)
+
     class Meta:
         model = User
 
-    username = factory.Sequence('User {}'.format)
-
     @factory.post_generation
     def password(self, create, extracted, **kwargs):
-        self.raw_password = extracted or 'default_password'
+        self.raw_password = 'default_password' if extracted is None else extracted
         self.set_password(self.raw_password)
         if create:
             self.save()
