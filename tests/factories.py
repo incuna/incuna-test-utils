d2e41e3c03e71919aeeaa72766f6c4037424d3c1
Kevin Etienne <kevin@incuna.com>
2014-11-19 12:33:21 +0000

    Add comment about User factory post_generation

--- tests/factories.py
+++ tests/factories.py
@@ -10,6 +10,7 @@ class UserFactory(factory.DjangoModelFactory):
 
     @factory.post_generation
     def password(self, create, extracted, **kwargs):
+        # By using this method password can never be set to `None`!
         self.raw_password = 'default_password' if extracted is None else extracted
         self.set_password(self.raw_password)
         if create:
