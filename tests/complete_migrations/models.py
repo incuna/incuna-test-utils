507b1a2992c4253c9b6931a4bc1ff1fed821cca7
Ian Foote <ian@incuna.com>
2015-01-29 17:58:48 +0000

    Add docstrings for Stub models

--- tests/complete_migrations/models.py
+++ tests/complete_migrations/models.py
@@ -2,4 +2,9 @@ from django.db import models
 
 
 class Stub(models.Model):
+    """
+    A stub model for testing check_migrations.
+
+    Has a complete set of migrations.
+    """
     name = models.CharField(max_length=255)
