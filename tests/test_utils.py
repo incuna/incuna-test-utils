33cad490034a40eb24dc04375c941da0b9b23da1
todd-dembrey <toddd@incuna.com>
2016-10-04 14:43:31 +0100

    Added get_field_by_name, another deprecated function from `Model._meta`
    @incuna/backend

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,6 +1,6 @@
 from incuna_test_utils import utils
 
-from .models import User
+from .models import User, ManyRelated
 
 
 class Parent(object):
@@ -52,3 +52,19 @@ def test_get_all_field_names():
     assert isinstance(fields, list)
     # Convert to set as list could complain about ordering in equality check
     assert set(fields) == {'id', 'email', 'name'}
+
+
+def test_get_field_by_name():
+    field = utils.get_field_by_name(User, 'email')
+    assert field[0] == User._meta.get_field('email')
+    assert not field[1]
+    assert field[2]
+    assert not field[3]
+
+
+def test_get_field_by_name_many_related():
+    field = utils.get_field_by_name(ManyRelated, 'relation')
+    assert field[0] == ManyRelated._meta.get_field('relation')
+    assert not field[1]
+    assert field[2]
+    assert field[3]
